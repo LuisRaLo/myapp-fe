5907f2442f769269d624fa1d92418ff213fdf0f9
Luis Enrique Ramírez López <luian.ramirez.12@gmail.com>
2023-10-24 00:24:12 -0600

    variables

--- src/utils/configs/init.ts
+++ src/utils/configs/init.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyB74QEZMEGJeVdz7BemU9JO0vQsakavM7c",
+  authDomain: "myapp-e5cbe.firebaseapp.com",
+  projectId: "myapp-e5cbe",
+  storageBucket: "myapp-e5cbe.appspot.com",
+  messagingSenderId: "327821887779",
+  appId: "1:327821887779:web:ead0060d9681901ec52496",
+  measurementId: "G-HK93QBFTC7",
 }
 
 // init firebase
